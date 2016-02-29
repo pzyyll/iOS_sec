602f676111d588f67f78a237807798df1a48285b
pzyyll <pzyyll@gmail.com>
2016-02-29 10:27:09 +0800

    ye
    ye

--- test/test/ViewController.swift
+++ test/test/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //var label: UILabel!
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func showYes(sender: UIButton) {
+        self.label!.text = "yes"
+    }
+ 
+    @IBAction func itemBtnMid(sender: UIBarButtonItem) {
+        self.label!.text = "MidItem"
+    }
 }
 
